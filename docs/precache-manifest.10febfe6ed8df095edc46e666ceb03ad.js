922c29ae9e9ae02f2be6f73409e6f70a335cde37
maulik-soni <mauliksoni37@gmail.com>
2020-05-21 13:06:05 +0530

    docs folder for github page

---
+++ docs/precache-manifest.10febfe6ed8df095edc46e666ceb03ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfc93237e44d78fe5667167a69056bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2607a9ef92b0b0ae1261",
+    "url": "/static/css/main.a779551e.chunk.css"
+  },
+  {
+    "revision": "3b153f580774826afcb6",
+    "url": "/static/js/2.5c3d4760.chunk.js"
+  },
+  {
+    "revision": "85a8e5fb5a5cdb30f526a0ca2cf06043",
+    "url": "/static/js/2.5c3d4760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2607a9ef92b0b0ae1261",
+    "url": "/static/js/main.88942301.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
