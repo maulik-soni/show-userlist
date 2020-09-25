3d6dca93fec8dcf3abb03b73df3c745801cb8ab0
maulik-soni <mauliksoni37@gmail.com>
2020-09-26 00:57:09 +0530

    new build

---
+++ docs/build/precache-manifest.f668600dfc677e3bc54dc1e9354f4017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2de838fb083ddac293deb28e521bbe5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887a541e388d59d0cb76",
+    "url": "/static/css/main.a779551e.chunk.css"
+  },
+  {
+    "revision": "54fcf5a7588e4440dee7",
+    "url": "/static/js/2.91d8be3f.chunk.js"
+  },
+  {
+    "revision": "85a8e5fb5a5cdb30f526a0ca2cf06043",
+    "url": "/static/js/2.91d8be3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887a541e388d59d0cb76",
+    "url": "/static/js/main.0b7acd39.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
